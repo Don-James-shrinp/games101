9ff6dba9d3c9de64dcee5947c1c7adb5775e08f3
Don Tizzy <3373213015@qq.com>
2022-07-14 21:19:12 +0800

    Update

--- Homework/pa0/pa0/main.cpp
+++ Homework/pa0/pa0/main.cpp
@@ -2,47 +2,23 @@
 #include<eigen3/Eigen/Core>
 #include<eigen3/Eigen/Dense>
 #include<iostream>
-
+#include <cmath>
 int main(){
-
-    // Basic Example of cpp
-    std::cout << "Example of cpp \n";
-    float a = 1.0, b = 2.0;
-    std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
-    std::cout << std::sqrt(b) << std::endl;
-    std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
-
-    // Example of vector
-    std::cout << "Example of vector \n";
-    // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
-    // vector output
-    std::cout << "Example of output \n";
-    std::cout << v << std::endl;
-    // vector add
-    std::cout << "Example of add \n";
-    std::cout << v + w << std::endl;
-    // vector scalar multiply
-    std::cout << "Example of scalar multiply \n";
-    std::cout << v * 3.0f << std::endl;
-    std::cout << 2.0f * v << std::endl;
-
-    // Example of matrix
-    std::cout << "Example of matrix \n";
-    // matrix definition
-    Eigen::Matrix3f i,j;
-    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
-    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
-    // matrix output
-    std::cout << "Example of output \n";
-    std::cout << i << std::endl;
-    // matrix add i + j
-    // matrix scalar multiply i * 2.0
-    // matrix multiply i * j
-    // matrix multiply vector i * v
-
+    double rad = 360*(acos(-1)/180);
+    Eigen::Matrix<double,2,2> rotate;
+    rotate(0,0) = cos(rad);
+    rotate(0,1) = -sin(rad);
+    rotate(1,0) = sin(rad);
+    rotate(1,1) = cos(rad);
+    std::cout<<rotate<<std::endl;
+    Eigen::Matrix<double,2,1> v;
+    v(0,0) = 5.0;
+    v(1,0) = 15.0;
+    std::cout<<rotate*v<<std::endl;
+    v = rotate*v;
+    Eigen::Matrix<double,2,1> translate;
+    translate(0,0) = 2.0f;
+    translate(0,1) = 2.0f;
+    std::cout<<v+translate<<std::endl;
     return 0;
 }
