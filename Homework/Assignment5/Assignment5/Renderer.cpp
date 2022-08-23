faf0243834e92bcbb8f2cc2151c49f34d4744032
Don James <1760892071@qq.com>
2022-08-23 15:51:03 +0800

    hw5-debug

--- Homework/Assignment5/Assignment5/Renderer.cpp
+++ Homework/Assignment5/Assignment5/Renderer.cpp
@@ -225,8 +225,8 @@ void Renderer::Render(const Scene& scene)
             // generate primary ray direction
             float x;
             float y;
-            x = (j + 0.5f) * scale * imageAspectRatio;
-            y = (i + 0.5f) * scale;
+            x = (i + 0.5f) * scale * imageAspectRatio;
+            y = (j + 0.5f) * scale;
             // TODO: Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
